e2b8122b0a3816c4271440078f3e117bcbacb6a1
darkborderman <reastw1234@gmail.com>
2018-11-09 02:35:00 +0800

    Basic frontend page setup

---
+++ static/assets/js/departmentBar.js
@@ -0,0 +1,13 @@
+let department={
+    name:'ncku-csie'
+};
+function addDepartment(department){
+
+    document.getElementById("department-bar").appendChild()
+}
+function generateReferer(){
+
+}
+function saveData(){
+    
+}
