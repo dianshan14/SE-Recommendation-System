71d9aa7152c2405f5fae0f308bc83418a52da228
e652424342007 <sam851214@gmail.com>
2018-11-13 00:45:14 +0800

    add send_mail

--- static/assets/js/index.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded',function(){
-    document.getElementById("register").addEventListener("click",function(){
-        window.location.href = `./register.html`;
-    })
-});
